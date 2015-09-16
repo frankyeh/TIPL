8284c194fa14d25e3d3022c2c666dd3401520f55
frankyeh <frank.yeh@gmail.com>
2015-09-15 22:26:41 -0400

    bug fix for color map

--- vis/color_map.hpp
+++ vis/color_map.hpp
@@ -52,7 +52,7 @@ struct color_map{
     std::vector<image::vector<3,float> > color;
 public:
     color_map(void):color(256){}
-    const image::vector<3,float>& operator[](unsigned int index){return color[std::min<unsigned int>(255,index)];}
+    const image::vector<3,float>& operator[](unsigned int index) const{return color[std::min<unsigned int>(255,index)];}
     void two_color(image::rgb_color from_color,image::rgb_color to_color)
     {
         color.resize(256);
@@ -60,8 +60,7 @@ public:
         {
             float findex = (float)index/255.0;
             for(unsigned char rgb_index = 0;rgb_index < 3;++rgb_index)
-                color[index][2-rgb_index] =
-                    ((float)to_color[rgb_index]*findex+(float)from_color[rgb_index]*(1.0-findex))/255.0;
+                color[index][rgb_index] = ((float)to_color[rgb_index]*findex+(float)from_color[rgb_index]*(1.0-findex))/255.0;
         }
     }
     void spectrum(void)
@@ -81,14 +80,13 @@ struct color_map_rgb{
     std::vector<image::rgb_color> color;
 public:
     color_map_rgb(void):color(256){}
-    const image::rgb_color& operator[](unsigned int index){return color[std::min<unsigned int>(255,index)];}
+    const image::rgb_color& operator[](unsigned int index) const{return color[std::min<unsigned int>(255,index)];}
     void two_color(image::rgb_color from_color,image::rgb_color to_color)
     {
         for(unsigned int index = 0;index < 256;++index)
         {
             for(unsigned char rgb_index = 0;rgb_index < 3;++rgb_index)
-                color[index][2-rgb_index] =
-                        std::min<short>(255,((float)to_color[rgb_index]*index+(float)from_color[rgb_index]*(255-index))/255.0);
+                color[index][rgb_index] = std::min<short>(255,((float)to_color[rgb_index]*index+(float)from_color[rgb_index]*(255-index))/255.0);
         }
     }
     void spectrum(void)
@@ -99,6 +97,43 @@ public:
             color[index][1] = image::color_spectrum_value(128,index);
             color[index][0] = image::color_spectrum_value(64,index);
         }
+    }
+};
+
+template<typename value_type>
+struct value_to_color{
+private:
+    value_type min_value,r;
+    image::color_map_rgb map;
+public:
+    value_to_color(void):min_value(0),r(1){}
+    void set_range(value_type min_value_,value_type max_value_)
+    {
+        min_value = min_value_;
+        max_value_ -= min_value_;
+        r = (max_value_ == 0.0) ? 1.0:256.0/max_value_;
+    }
+    void set_color_map(const image::color_map_rgb& rhs)
+    {
+        map = rhs;
+    }
+    const image::rgb_color& operator[](value_type value)const
+    {
+        value -= min_value;
+        value *= r;
+        int ivalue = std::floor(value);
+        if(ivalue < 0)
+            ivalue = 0;
+        if(ivalue > 255)
+            ivalue = 255;
+        return map[ivalue];
+    }
+    template<typename value_image_type,typename color_image_type>
+    void convert(const value_image_type& I1,color_image_type& I2) const
+    {
+        I2.resize(I1.geometry());
+        for(unsigned int i = 0;i < I1.size();++i)
+            I2[i] = (*this)[I1[i]];
     }
 };
 
