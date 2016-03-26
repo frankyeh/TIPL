4de4a172c7ef828520ecfd3bf0afb20aede6daae
frankyeh <frank.yeh@gmail.com>
2016-03-26 09:11:51 -0400

    cont'

--- reg/reg.hpp
+++ reg/reg.hpp
@@ -20,25 +20,12 @@ private:
 
 private:
     std::shared_ptr<image::reg::bfnorm_mapping<value_type,3> > bnorm_data;
-    std::shared_ptr<std::future<void> > reg_thread;
-    unsigned char terminated;
     int prog;
-    void clear_thread(void)
-    {
-        if(reg_thread.get())
-        {
-            terminated = 1;
-            reg_thread->wait();
-            reg_thread.reset();
-        }
-    }
+
+
 
 public:
     normalization(void):prog(0),has_T(false){}
-    ~normalization(void)
-    {
-        clear_thread();
-    }
 public:
     void update_affine(void)
     {
@@ -50,11 +37,11 @@ public:
     const image::transformation_matrix<value_type>& get_T(void) const{return T;}
     const image::transformation_matrix<value_type>& get_iT(void) const{return iT;}
     const image::affine_transform<value_type> get_arg(void) const{return arg;}
-    image::affine_transform<value_type> get_arg(void){return arg;}
+    image::affine_transform<value_type>& get_arg(void){return arg;}
     template<typename value_type>
     void set_arg(const value_type& rhs){arg = rhs;update_affine();}
 public:
-    template<typename image_type,typename vector_type>
+    template<typename image_type,typename vector_type,typename terminate_type>
     void run_reg(const image_type& from,
                  const vector_type& from_vs_,
                  const image_type& to,
@@ -62,9 +49,9 @@ public:
                  int factor,
                  reg_cost_type cost_function,
                  image::reg::reg_type reg_type,
+                 terminate_type& terminated,
                  int thread_count = std::thread::hardware_concurrency())
     {
-        terminated = false;
         has_T = false;
         from_geo = from.geometry();
         to_geo = to.geometry();
@@ -100,23 +87,6 @@ public:
         image::reg::bfnorm(*bnorm_data.get(),new_from,to,thread_count,terminated,prog);
         prog = 16;
     }
-    template<typename image_type,typename vector_type>
-    void run_background(const image_type& from,
-                 const vector_type& from_vs_,
-                 const image_type& to,
-                 const vector_type& to_vs_,
-                 int factor,
-                 reg_cost_type cost_function,
-                 image::reg::reg_type reg_type,
-                 int thread_count = std::thread::hardware_concurrency())
-    {
-        clear_thread();
-        reg_thread.reset(new std::future<void>(std::async(std::launch::async,
-            [this,&from,&from_vs_,&to,&to_vs_,factor,cost_function,reg_type,thread_count]()
-        {
-            run_reg(from,from_vs_,to,to_vs_,1,cost_function,reg_type,thread_count);
-        })));
-    }
 
     int get_prog(void)const{return prog+2;}
     template<typename vtype,typename vtype2>
@@ -126,7 +96,7 @@ public:
             update_affine();
         vtype2 pos;
         T(index,pos);// from -> new_from
-        if(prog)
+        if(prog > 0)
         {
             pos += 0.5;
             (*bnorm_data.get())(image::vector<3,int>(pos[0],pos[1],pos[2]),out);
