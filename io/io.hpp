abeec47332bb6009f34096af32497837a2ba0ce6
frankyeh <frank.yeh@gmail.com>
2021-09-28 20:24:00 -0400

    Update io.hpp

--- io/io.hpp
+++ io/io.hpp
@@ -19,17 +19,16 @@ namespace io
 class volume{
 public:
     std::vector<std::shared_ptr<dicom> > dicom_reader;
-    std::vector<std::shared_ptr<nifti> > nifti_reader;
     float orientation_matrix[9];
     tipl::shape<3> dim;
-    tipl::vector<3,float> vs;
+    tipl::vector<3> vs;
     uint8_t dim_order[3]; // used to rotate the volume to axial view
     uint8_t flip[3];        // used to rotate the volume to axial view
+    std::string error_msg;
 
     void free_all(void)
     {
         dicom_reader.clear();
-        nifti_reader.clear();
     }
     void change_orientation(bool x,bool y,bool z)
     {
@@ -41,95 +40,66 @@ public:
             if(xyz[dim_order[index]])
                 flip[index] = !flip[index];
     }
-
-
-
 public:
     ~volume(void){free_all();}
     const std::shared_ptr<dicom> get_dicom(unsigned int index) const{return dicom_reader[index];}
-    const std::shared_ptr<nifti> get_nifti(unsigned int index) const{return nifti_reader[index];}
-    const shape<3>& geo(void) const{return dim;}
+    const shape<3>& shape(void) const{return dim;}
     void get_voxel_size(tipl::vector<3,float>& voxel_size) const
     {
         voxel_size = vs;
     }
-
     template<typename vector_type>
     void get_image_row_orientation(vector_type image_row_orientation) const
     {
         std::copy(orientation_matrix,orientation_matrix+9,image_row_orientation);
     }
-
-
-    template<typename file_name_type>
-    bool load_from_file(const file_name_type& file_name)
+    bool load_from_files(const std::vector<std::string>& files)
     {
-        std::shared_ptr<dicom> dicom_header(new dicom);
-        if (dicom_header->load_from_file(file_name))
-        {
-            dicom_header->get_image_dimension(dim);
-            dicom_header->get_voxel_size(vs);
-            dicom_header->get_image_orientation(orientation_matrix);
-            free_all();
-            dicom_reader.push_back(dicom_header);
-            tipl::get_orientation(3,orientation_matrix,dim_order,flip);
-            tipl::reorient_vector(vs,dim_order);
-            tipl::reorient_matrix(orientation_matrix,dim_order,flip);
-            return true;
-        }
-        std::shared_ptr<nifti> nifti_header(new nifti);
-        if (nifti_header->load_from_file(file_name))
-        {
-            nifti_header->get_image_dimension(dim);
-            nifti_header->get_voxel_size(vs);
-            nifti_header->get_image_orientation(orientation_matrix);
-            free_all();
-            nifti_reader.push_back(nifti_header);
-            tipl::get_inverse_orientation(3,orientation_matrix,dim_order,flip);
-            change_orientation(true,true,false);
-            // from +x = Right  +y = Anterior +z = Superior
-            // to +x = Left  +y = Posterior +z = Superior
-            tipl::reorient_vector(vs,dim_order);
-            tipl::reorient_matrix(orientation_matrix,dim_order,flip);
-            return true;
-        }
-        return false;
-    }
-    template<typename string_list_type>
-    bool load_from_files(const string_list_type& files,unsigned int count)
-    {
-        if(count == 1)
-            return load_from_file(files[0]);
+        if(files.empty())
+            return false;
         free_all();
-        for (unsigned int index = 0;index < count;++index)
+        std::vector<int> image_num;
+        float r0[9];
+        for (unsigned int index = 0;index < files.size();++index)
         {
             std::shared_ptr<dicom> d(new dicom);
-            if (d->load_from_file(files[index]))
+            if (!d->load_from_file(files[index]))
             {
-                dicom_reader.push_back(d);
-                continue;
-            }
-            std::shared_ptr<nifti> n(new nifti);
-            if (n->load_from_file(files[index].c_str()))
-            {
-                nifti_reader.push_back(n);
-            }
-        }
-        if(!dicom_reader.empty())
-        {
-            // remove first slice if it has different imag orientation
-            {
-                float r1[9],r2[9];
-                dicom_reader[0]->get_image_orientation(r1);
-                dicom_reader[1]->get_image_orientation(r2);
-                if(r1[0] != r2[0])
-                    dicom_reader.erase(dicom_reader.begin());
-            }
-            if(dicom_reader.size() < 2)
+                error_msg = "failed to read ";
+                error_msg += files[index];
                 return false;
+            }
+            if(index)
+            {
+                float rn[9];
+                d->get_image_orientation(rn);
+                if(rn[0] != r0[0])
+                {
+                    error_msg = "inconsistent image orientation at ";
+                    error_msg += files[index];
+                    return false;
+                }
+            }
+            else
+                d->get_image_orientation(r0);
+            // get image sequence
+            image_num.push_back(0);
+            std::istringstream(d->get_image_num()) >> image_num.back();
+            dicom_reader.push_back(d);
+        }
+        // sort dicom according to the image num
+        {
+            auto order = tipl::arg_sort(image_num.size(),[&](int i,int j){return image_num[i] < image_num[j];});
+            std::vector<std::shared_ptr<dicom> > new_dicom_reader(order.size());
+            for(size_t i = 0;i < order.size();++i)
+                new_dicom_reader[i] = dicom_reader[order[i]];
+            new_dicom_reader.swap(dicom_reader);
+        }
+
+        {
             dim = tipl::shape<3>(dicom_reader.front()->width(),
-                                    dicom_reader.front()->height(),
-                                    dicom_reader.size());
+                                 dicom_reader.front()->height(),
+                                 dicom_reader.size());
             dicom_reader.front()->get_voxel_size(vs);
             dicom_reader.front()->get_image_orientation(orientation_matrix);
             if(vs[2] == 0.0f)
@@ -148,62 +118,37 @@ public:
             tipl::get_orientation(3,orientation_matrix,dim_order,flip);
 
         }
-        else
-            if(!nifti_reader.empty())
-            {
-                dim = tipl::shape<3>(nifti_reader.front()->width(),
-                                        nifti_reader.front()->height(),
-                                        nifti_reader.size());
-                nifti_reader.front()->get_voxel_size(vs);
-                nifti_reader.front()->get_image_orientation(orientation_matrix);
-                tipl::get_inverse_orientation(3,orientation_matrix,dim_order,flip);
-                change_orientation(true,true,false);
-                // from +x = Right  +y = Anterior +z = Superior
-                // to +x = Left  +y = Posterior +z = Superior
-            }
-            else
-                return false;
         tipl::reorient_vector(vs,dim_order);
         tipl::reorient_matrix(orientation_matrix,dim_order,flip);
         return true;
     }
     template<typename image_type>
-    void save_to_image(image_type& source) const
+    void get_untouched_image(image_type& source) const
     {
-        typedef typename image_type::value_type value_type;
-        tipl::image<value_type,3> buffer;
-        if(!dicom_reader.empty())
-        {
-            if(dicom_reader.size() == 1)
-                *dicom_reader.front() >> buffer;
-            else
-            {
-                buffer.resize(dim);
-                for(size_t index = 0;index < dicom_reader.size();++index)
-                    dicom_reader[index]->save_to_buffer(&*buffer.begin()+index*dim.plane_size(),dim.plane_size());
-            }
-            tipl::reorder(buffer,source,dim_order,flip);
+        if(dicom_reader.empty())
             return;
-        }
-        if(!nifti_reader.empty())
+        if(dicom_reader.size() == 1)
+            *dicom_reader.front() >> source;
+        else
         {
-            if(nifti_reader.size() == 1)
-                nifti_reader.front()->get_untouched_image(buffer);
-            else
-            {
-                buffer.resize(dim);
-                for(size_t index = 0;index < nifti_reader.size();++index)
-                    nifti_reader[index]->save_to_buffer(&*buffer.begin()+index*dim.plane_size(),dim.plane_size());
-            }
-            tipl::reorder(buffer,source,dim_order,flip);
-            return;
+            source.resize(dim);
+            for(size_t index = 0;index < dicom_reader.size();++index)
+                dicom_reader[index]->save_to_buffer(&*source.begin()+index*dim.plane_size(),dim.plane_size());
         }
     }
 
     template<typename image_type>
-    const volume& operator>>(image_type& source) const
+    void save_to_image(image_type& I) const
     {
-        save_to_image(source);
+        tipl::image<typename image_type::value_type,3> buffer;
+        get_untouched_image(buffer);
+        tipl::reorder(buffer,I,dim_order,flip); // to LPS
+    }
+
+    template<typename image_type>
+    const volume& operator>>(image_type& I) const
+    {
+        save_to_image(I);
         return *this;
     }
 };
