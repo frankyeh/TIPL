6f9746e4e1ef0385959fdf22d19f7bfcb74c17b3
frankyeh <frank.yeh@gmail.com>
2022-01-20 15:35:50 -0500

    Update def.hpp

--- utility/def.hpp
+++ utility/def.hpp
@@ -1,10 +1,20 @@
+#ifndef TIPL_DEF
+#define TIPL_DEF
+
 #ifdef INCLUDE_NLOHMANN_JSON_HPP_
 #define USING_XEUS_CLING
 #endif
 
 #ifdef __CUDACC__
 #define __DEVICE_HOST__ __device__ __host__
+#define __INLINE__ __forceinline__ __device__ __host__
+#include <thrust/sequence.h>
+#include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/transform.h>
 #else
 #define __DEVICE_HOST__
+#define __INLINE__ inline
 #endif
 
+#endif//TIPL_DEF
