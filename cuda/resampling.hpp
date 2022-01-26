0a05f0542da332330910ac63515b209007449107
frankyeh <frank.yeh@gmail.com>
2022-01-26 01:17:03 -0500

    Update resampling.hpp

--- cuda/resampling.hpp
+++ cuda/resampling.hpp
@@ -1,28 +1,30 @@
 #ifndef CUDA_RESAMPLE_HPP
 #define CUDA_RESAMPLE_HPP
 
+#include "../utility/basic_image.hpp"
 
 #ifdef __CUDACC__
 
 #include "../numerical/interpolation.hpp"
-
 namespace tipl{
 
-template<tipl::interpolation itype,typename T,typename U>
-__global__ void resample_cuda_kernel(const T* from,T* to,const U* trans_,tipl::shape<3> from_shape)
+template<tipl::interpolation itype,typename T1,typename T2,typename U>
+__global__ void resample_cuda_kernel(tipl::const_pointer_image<3,T1> from,
+                                      tipl::pointer_image<3,T2> to,
+                                      U trans)
 {
-    const tipl::transformation_matrix<U>& trans = *reinterpret_cast<const tipl::transformation_matrix<U>* >(trans_);
     tipl::pixel_index<3> index(blockIdx.x,blockIdx.y,threadIdx.x,tipl::shape<3>(gridDim.x,gridDim.y,blockDim.x));
     tipl::vector<3> v;
     trans(index,v);
-    tipl::estimate<itype>(tipl::make_image(from,from_shape),v,to[index.index()]);
+    tipl::estimate<itype>(from,v,to[index.index()]);
 }
 
+
 template<tipl::interpolation itype = linear,typename T,typename T2,typename U>
-void resample_cuda(const T& dfrom,T2& dto,const U& trans,bool sync = true)
+inline void resample_cuda(const T& from,T2& to,const U& trans,bool sync = true)
 {
-    resample_cuda_kernel<itype><<<dim3(dto.width(),dto.height()),dto.depth()>>>
-        (dfrom.get(),dto.get(),tipl::device_memory<typename U::value_type>(trans).get(),dfrom.shape());
+    resample_cuda_kernel<itype,typename T::value_type,typename T2::value_type>
+            <<<dim3(to.width(),to.height()),to.depth()>>>(from,to,trans);
     if(sync)
         cudaDeviceSynchronize();
 }
