f8d5161725cddbcb55eee68f522850208bcd7949
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-03-04 17:47:56 -0500

    Update tipl.hpp

--- tipl.hpp
+++ tipl.hpp
@@ -1,28 +1,15 @@
 #ifndef TIPL_HPP
 #define TIPL_HPP
 /*
-Copyright (c) 2010-2022 Fang-Cheng Yeh
+Copyright (c) 2010-2023 Fang-Cheng Yeh
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
+TIPL library is shared under one of the following licenses
+(1) GPLv4 license (https://www.gnu.org/licenses/gpl-3.0.en.html)
+(2) A propriety license allowing close-source usage.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL FANG-CHENG YEH BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
 #include "def.hpp"
 
 #include "utility/basic_image.hpp"
