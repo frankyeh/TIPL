623a78d5e845631b80641c5b86e71ccdef3b98d5
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2023-04-17 09:15:06 -0400

    Update qt_ext.hpp

--- vis/qt_ext.hpp
+++ vis/qt_ext.hpp
@@ -7,9 +7,65 @@
 #include <QScrollBar>
 
 
+
+// show image on scene and keep the original scroll bar position if zoom in/out
+inline void operator<<(QGraphicsScene& scene,QImage I)
+{
+    float vb_ratio(0),hb_ratio(0);
+    QScrollBar* vb(nullptr),*hb(nullptr);
+    if(scene.views().size() && int(float(scene.sceneRect().width())/float(scene.sceneRect().height())*100.0f) ==
+           int(float(I.width())/float(I.height())*100.0f))
+    {
+        vb = scene.views()[0]->verticalScrollBar();
+        hb = scene.views()[0]->horizontalScrollBar();
+        if(vb->isVisible())
+            vb_ratio = float((vb->value()+vb->pageStep()/2))/float(vb->maximum()+vb->pageStep());
+        if(hb->isVisible())
+            hb_ratio = float((hb->value()+hb->pageStep()/2))/float(hb->maximum()+hb->pageStep());
+    }
+    {
+        scene.setSceneRect(0, 0, I.width(),I.height());
+        scene.clear();
+        scene.addPixmap(tipl::qt::image2pixelmap(I));
+#ifdef WIN32
+        scene.addPixmap(QPixmap::fromImage(I));
+#else
+        //For Mac, the endian system is BGRA and all QImage needs to be converted.
+        scene.addPixmap(QPixmap::fromImage(I.convertToFormat(QImage::Format_ARGB32)));
+#endif
+    }
+
+    if(vb_ratio != 0.0f)
+        vb->setValue(int(vb_ratio*(vb->maximum()+vb->pageStep())-vb->pageStep()/2));
+    if(hb_ratio != 0.0f)
+        hb->setValue(int(hb_ratio*(hb->maximum()+hb->pageStep())-hb->pageStep()/2));
+}
+inline QImage& operator << (QImage& image,const tipl::color_image& I)
+{
+   return image = QImage((unsigned char*)&*I.begin(),
+                  I.width(),I.height(),QImage::Format_RGB32).copy();
+
+}
+inline QImage operator << (QImage&&,const tipl::color_image& I)
+{
+   return QImage((unsigned char*)&*I.begin(),
+                  I.width(),I.height(),QImage::Format_RGB32).copy();
+
+}
+
+
 namespace tipl{
 namespace qt{
 
+inline QPixmap image2pixelmap(const QImage &I)
+{
+    #ifdef WIN32
+        return QPixmap::fromImage(I);
+    #else
+        //For Mac, the endian system is BGRA and all QImage needs to be converted.
+        return QPixmap::fromImage(I.convertToFormat(QImage::Format_ARGB32));
+    #endif
+}
 
 
 
@@ -229,57 +285,9 @@ QImage draw_regions(const std::vector<image_type>& region_masks,
 }
 
 
-inline QPixmap image2pixelmap(const QImage &I)
-{
-    #ifdef WIN32
-        return QPixmap::fromImage(I);
-    #else
-        //For Mac, the endian system is BGRA and all QImage needs to be converted.
-        return QPixmap::fromImage(I.convertToFormat(QImage::Format_ARGB32));
-    #endif
-}
-
 }//qt
 }//tipl
 
-// show image on scene and keep the original scroll bar position if zoom in/out
-inline void operator<<(QGraphicsScene& scene,QImage I)
-{
-    float vb_ratio(0),hb_ratio(0);
-    QScrollBar* vb(nullptr),*hb(nullptr);
-    if(scene.views().size() && int(float(scene.sceneRect().width())/float(scene.sceneRect().height())*100.0f) ==
-           int(float(I.width())/float(I.height())*100.0f))
-    {
-        vb = scene.views()[0]->verticalScrollBar();
-        hb = scene.views()[0]->horizontalScrollBar();
-        if(vb->isVisible())
-            vb_ratio = float((vb->value()+vb->pageStep()/2))/float(vb->maximum()+vb->pageStep());
-        if(hb->isVisible())
-            hb_ratio = float((hb->value()+hb->pageStep()/2))/float(hb->maximum()+hb->pageStep());
-    }
-    {
-        scene.setSceneRect(0, 0, I.width(),I.height());
-        scene.clear();
-        scene.addPixmap(tipl::qt::image2pixelmap(I));
-    }
-
-    if(vb_ratio != 0.0f)
-        vb->setValue(int(vb_ratio*(vb->maximum()+vb->pageStep())-vb->pageStep()/2));
-    if(hb_ratio != 0.0f)
-        hb->setValue(int(hb_ratio*(hb->maximum()+hb->pageStep())-hb->pageStep()/2));
-}
-inline QImage& operator << (QImage& image,const tipl::color_image& I)
-{
-   return image = QImage((unsigned char*)&*I.begin(),
-                  I.width(),I.height(),QImage::Format_RGB32).copy();
-
-}
-inline QImage operator << (QImage&&,const tipl::color_image& I)
-{
-   return QImage((unsigned char*)&*I.begin(),
-                  I.width(),I.height(),QImage::Format_RGB32).copy();
-
-}
 
 
 #endif//TIPL_QT_EXT_HPP
