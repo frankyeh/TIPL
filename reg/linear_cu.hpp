067080209840e793094c65c9c7079c1a438a9766
Yeh <frank.yeh@gmail.com>
2026-03-27 15:06:55 -0400

    Create linear_cu.hpp

---
+++ reg/linear_cu.hpp
@@ -0,0 +1,19 @@
+#include "TIPL/prog.hpp"
+#include "TIPL/reg/linear.hpp"
+#ifdef __CUDACC__
+namespace tipl::reg
+{
+
+template
+float linear_reg_imp<tipl::out,false,unsigned char,3>(std::true_type,
+            std::shared_ptr<tipl::reg::linear_reg_runner<3,unsigned char,tipl::out> > reg,
+                        tipl::reg::cost_type cost_type,
+                        bool& terminated);
+template
+float linear_reg_imp<tipl::out,true,unsigned char,3>(std::true_type,
+            std::shared_ptr<tipl::reg::linear_reg_runner<3,unsigned char,tipl::out> > reg,
+                        tipl::reg::cost_type cost_type,
+                        bool& terminated);
+
+}
+#endif//__CUDACC__
