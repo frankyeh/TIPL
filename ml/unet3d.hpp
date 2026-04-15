29b9674716a3242a3a5cdf3dc1611d24d92f087a
Yeh <frank.yeh@gmail.com>
2026-04-14 22:30:10 -0400

    Update unet3d.hpp

--- ml/unet3d.hpp
+++ ml/unet3d.hpp
@@ -101,20 +101,59 @@ tipl::image<3> defragment4d(image_type& this_image,float prob_threshold)
 }
 
 template<typename U,typename V>
-tipl::image<3> postproc_actions(const U& eval_output,const V& raw_image_shape,
-                                tipl::transformation_matrix<float,3> trans,size_t model_out_count)
+tipl::image<3> postproc_actions(const U& eval_output,
+                                const V& raw_image_shape,
+                                tipl::transformation_matrix<float,3> trans,
+                                size_t model_out_count,
+                                bool has_bg_channel)
 {
-
     tipl::shape<3> dim_from(eval_output.shape().divide(tipl::shape<3>::z,model_out_count)),
                    dim_to(raw_image_shape);
     tipl::image<3> label_prob(dim_to.multiply(tipl::shape<3>::z,model_out_count));
     trans.inverse();
+
+    // 1. Resample each channel to the raw image space
     tipl::par_for(model_out_count,[&](int i)
     {
         auto from = eval_output.alias(dim_from.size()*i,dim_from);
         auto to = label_prob.alias(dim_to.size()*i,dim_to);
         tipl::resample(from,to,trans);
     },model_out_count);
+
+    // 2. Normalize probabilities across all channels (tissue + potential bg)
+    size_t single_channel_size = dim_to.size();
+    tipl::par_for(single_channel_size, [&](size_t i)
+    {
+        float sum = 0.0f;
+        for(size_t c = 0; c < model_out_count; ++c) {
+            sum += label_prob[c * single_channel_size + i];
+        }
+
+        // Avoid division by zero in out-of-bounds padded regions
+        if(sum > 0.0f)
+        {
+            float inv_sum = 1.0f / sum;
+            for(size_t c = 0; c < model_out_count; ++c) {
+                label_prob[c * single_channel_size + i] *= inv_sum;
+            }
+        }
+    });
+
+    // 3. Remove background channel if it exists
+    if (has_bg_channel && model_out_count > 1)
+    {
+        size_t new_total_size = single_channel_size * (model_out_count - 1);
+        size_t shift = single_channel_size;
+
+        // Shift memory left by one whole channel size
+        for(size_t i = 0; i < new_total_size; ++i) {
+            label_prob[i] = label_prob[i + shift];
+        }
+
+        // Resize to discard the trailing memory block
+        label_prob.resize(dim_to.multiply(tipl::shape<3>::z, model_out_count - 1));
+    }
+
     return label_prob;
 }
 
@@ -151,10 +190,12 @@ inline auto soft_mask(const T& label)
 
 class tissue_seg{
 public:
-    std::shared_ptr<unet3d<tipl::progress>> unet;
+    bool deep_supervision = false;
+    std::shared_ptr<network> unet;
     std::string error_msg;
     tipl::vector<3> vs;
     tipl::shape<3> dim;
+    int num_tissue_channels = 5;
 
     template<typename reader>
     bool load_model(const std::string& file_name)
@@ -171,28 +212,40 @@ public:
         std::vector<std::vector<int> > features_up;
         std::vector<int> kernel_size;
         parse_feature_string(feature_string,param[0],features_down,features_up,kernel_size);
-        unet.reset(new unet3d<tipl::progress>(features_down,features_up,kernel_size,param[0],param[1]));
+        if(in.has("errors"))
+        {
+            unet.reset(new unet3d<tipl::progress,unet_version::deep_supervision>(features_down,features_up,kernel_size,param[0],param[1]));
+            deep_supervision = true;
+        }
+        else
+            unet.reset(new unet3d<tipl::progress>(features_down,features_up,kernel_size,param[0],param[1]));
 
         if(!in.read("dimension",dim) || !in.read("voxel_size",vs))
             return error_msg = "cannot read dimension and voxel size",false;
         unet->init_image(dim); // the dim value will be changed
         dim = unet->dim;
         int id = 0;
-        for(auto& param : unet->parameters())
+        for(auto& p : unet->parameters())
         {
-            if(!param.second)
+            if(!p.second)
                 continue;
-            auto name = "tensor"+std::to_string(id++);
-            if(!in.has(name))
-                return error_msg = "tensor structure mismatch",false;
-            unsigned int r,c;
-            if(!in.get_col_row(name,r,c) || r*c != param.second)
-                return error_msg = "tensor size mismatch",false;
-            if(!in.read(name,param.first,param.first+param.second))
-                return error_msg = "error reading tensor structure",false;
+
+            std::string name = "tensor" + std::to_string(id++);
+
+            if(!in.has(name.c_str()))
+                return error_msg = "tensor structure mismatch (missing " + name + ")", false;
+
+            unsigned int tr, tc;
+            if(!in.get_col_row(name.c_str(), tr, tc) || tr * tc != p.second)
+                return error_msg = "tensor size mismatch in " + name + " " +
+                        std::to_string(tr * tc) + " vs " + std::to_string(p.second), false;
+
+            if(!in.read(name.c_str(), p.first, p.first + p.second))
+                return error_msg = "error reading tensor structure " + name, false;
         }
         return true;
     }
+
 public:
     template<typename image_type>
     bool forward(const image_type& raw_image,
@@ -210,32 +263,40 @@ public:
                                         dim,vs,trans,match_resolution,match_fov);
         if(dim != input_image.shape())
             return false;
-        unet->prog = &prog;
+        //unet->prog = &prog;
         auto ptr = unet->forward(input_image.data());
         if(ptr == nullptr)
             return false;
         prog(0,4);
+
         auto evaluate_output = tipl::make_image(ptr,unet->dim.multiply(tipl::shape<3>::z,unet->out_channels_));
-        auto label_prob = postproc_actions(evaluate_output,raw_image.shape(),trans,unet->out_channels_);
-        auto label_prob_4d = tipl::make_image(label_prob.data(),raw_image.shape().expand(unet->out_channels_));
+
+        auto label_prob = postproc_actions(evaluate_output, raw_image.shape(), trans, unet->out_channels_, deep_supervision);
+
+        num_tissue_channels = deep_supervision ? (unet->out_channels_ - 1) : unet->out_channels_;
+
+        auto label_prob_4d = tipl::make_image(label_prob.data(), raw_image.shape().expand(num_tissue_channels));
         prog(2,4);
-        auto fg_prob = tipl::ml3d::defragment4d(label_prob_4d,prob_threshold);
+
+        tipl::image<3> fg_prob = tipl::ml3d::defragment4d(label_prob_4d, prob_threshold);
         prog(3,4);
 
         tipl::image<3,unsigned char> I(fg_prob.shape());
         size_t s = fg_prob.size();
-        for(size_t pos = 0;pos < s;++pos)
+        for(size_t pos = 0; pos < s; ++pos)
         {
             if(fg_prob[pos] <= prob_threshold)
                 continue;
             float m = label_prob[pos];
             unsigned char max_label = 1;
-            for(size_t i = pos+s,label = 2;i < label_prob.size();i += s,++label)
+            for(size_t i = pos+s, label = 2; i < label_prob.size(); i += s, ++label)
+            {
                 if(label_prob[i] > m)
                 {
                     m = label_prob[i];
                     max_label = label;
                 }
+            }
             I[pos] = max_label;
         }
         I.swap(label);
@@ -243,9 +304,6 @@ public:
         return true;
     }
 };
-
-
-
 
 
 }//ml3d
