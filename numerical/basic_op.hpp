2a8c115833362c1634423dd607b33ab9c655721e
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2026-05-16 19:05:51 -0400

    Update basic_op.hpp

--- numerical/basic_op.hpp
+++ numerical/basic_op.hpp
@@ -1507,11 +1507,12 @@ auto argmax(const ImageType& label_prob,const tipl::shape<3>& dim,const mask_typ
 }
 
 template<typename U>
-auto remove_channel(U& label_prob,const shape<3>& image_dim,size_t bg_channel = 0)
+void remove_channel(U& label_prob,const shape<3>& image_dim,size_t bg_channel = 0)
 {
-    std::copy(label_prob.begin() + (bg_channel+1)*image_dim.size(), label_prob.end(), label_prob.begin() + bg_channel*image_dim.size());
-    label_prob.resize(image_dim.multiply(tipl::shape<3>::z, label_prob.depth()/image_dim[2]-1));
-}
+    size_t s = image_dim.size();
+    std::copy(label_prob.begin() + (bg_channel+1)*s,label_prob.end(),label_prob.begin() + bg_channel*s);
+    label_prob.resize(image_dim.multiply(tipl::shape<3>::z,label_prob.depth()/image_dim[2]-1));
+}
 
 template<typename type>
 inline void change_endian(type& data)
